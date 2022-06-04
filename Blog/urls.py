1401de8a0f8f4a29b4348ccb5fad91abd3ca0806
mohammad <mohammadrzapodineh@gmail.com>
2022-06-04 05:11:26 -0700

    Create Post Share View In Project

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . views import PostList, post_detail
+from . views import PostList, post_detail, post_share
 
 app_name = 'Blog'
 urlpatterns = [
     path('', PostList.as_view(), name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post_slug>/', post_detail, name='post_detail')
+    path('<int:year>/<int:month>/<int:day>/<slug:post_slug>/', post_detail, name='post_detail'),
+    path('<int:post_id>/share/', post_share, name='post_share'),
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import SharePostForm
+from django.core.mail import send_mail
 
 
 class PostList(ListView):
@@ -22,3 +24,18 @@ def post_detail(request, year, month, day, post_slug):
     context = {'post': post}
     return render(request, 'Blog/post_detail.html', context)
 
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='publish')
+    share_form = SharePostForm(data=request.POST or None)
+    sent = False
+    if share_form.is_valid():
+        comment = share_form.cleaned_data.get('comment')
+        to = share_form.cleaned_data.get('to')
+        subject = f'You Get A  Share Post {post.title}'
+        post_url = request.build_absolute_uri(post.get_absolute_url())
+        message = f'please See This Post {post_url} your Comments {comment}'
+        send_mail(subject, message, 'admin@Smartblog.com', [to])
+        sent = True
+    context = {'post': post, 'form': share_form, 'sent': sent}
+    return render(request, 'Blog/post_share.html', context)
